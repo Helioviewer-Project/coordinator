6f24871945894d0ac11340905a0cfe104ed1607f
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2024-05-16 13:20:55 -0400

    Add normalize hpc endpoint

--- app/frames.py
+++ app/frames.py
@@ -1,5 +1,17 @@
-from sunpy.coordinates.frames import Helioprojective
+from sunpy.coordinates.frames import Helioprojective, HeliographicStonyhurst
+import astropy.units as u
 
+def get_earth_frame(obstime: str):
+    """
+    Returns the Helioprojective frame as seen from earth at the
+    given observation time
+
+    Parameters
+    ----------
+    obstime: str
+        Observation time with both date and time.
+    """
+    return Helioprojective(observer="earth", obstime=obstime)
 
 def get_helioviewer_frame(obstime: str):
     """
@@ -12,4 +24,9 @@ def get_helioviewer_frame(obstime: str):
         Observation time with both date and time. Supports
         all formats supported by sunpy
     """
-    return Helioprojective(observer="earth", obstime=obstime)
+    # Start with earth's frame of reference
+    earth_frame = get_earth_frame(obstime)
+    # Adjust the distance parameter to 1AU
+    hv_observer = HeliographicStonyhurst(earth_frame.observer.lon, earth_frame.observer.lat, 1*u.au, obstime=obstime)
+    # Return a new frame with Helioviewer's point of view as the observer
+    return Helioprojective(observer=hv_observer, obstime=obstime)
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, ValidationError, ConfigDict
 from flask import Flask, request
 from hgs2hpc import hgs2hpc
+from normalizer import normalize_hpc
 
 app = Flask("Helioviewer")
 
@@ -26,6 +27,21 @@ def _hgs2hpc():
     try:
         params = Hgs2HpcQueryParameters(**request.args)
         coord = hgs2hpc(params.lat, params.lon, params.obstime)
+        return {"x": coord.Tx.value, "y": coord.Ty.value}
+    except ValidationError as e:
+        return e.errors(), 400
+
+
+class NormalizeHpcQueryParameters(HvBaseModel):
+    x: float
+    y: float
+    obstime: str
+
+@app.route("/hpc", methods=["GET"])
+def _normalize_hpc():
+    try:
+        params = NormalizeHpcQueryParameters(**request.args)
+        coord = normalize_hpc(params.x, params.y, params.obstime)
         return {"x": coord.Tx.value, "y": coord.Ty.value}
     except ValidationError as e:
         return e.errors(), 400
