880251f114e192228c87a6487831b0ec83b47ef3
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2024-05-16 14:20:15 -0400

    Ignore E501

--- app/frames.py
+++ app/frames.py
@@ -30,10 +30,7 @@ def get_helioviewer_frame(obstime: str):
     earth_frame = get_earth_frame(obstime)
     # Adjust the distance parameter to 1AU
     hv_observer = HeliographicStonyhurst(
-        earth_frame.observer.lon,
-        earth_frame.observer.lat,
-        1 * u.au,
-        obstime=obstime
+        earth_frame.observer.lon, earth_frame.observer.lat, 1 * u.au, obstime=obstime
     )
     # Return a new frame with Helioviewer's point of view as the observer
     return Helioprojective(observer=hv_observer, obstime=obstime)
