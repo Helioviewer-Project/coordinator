059ae7ad101da344f8f62fa10964f71abcb2beb7
Daniel Garcia Briseno <daniel.garciabriseno@nasa.gov>
2025-02-06 12:00:06 -0500

    Update deprecated function

--- app/normalizer.py
+++ app/normalizer.py
@@ -2,10 +2,10 @@ from astropy.coordinates import SkyCoord
 from astropy.time import Time
 import astropy.units as u
 from sunpy.coordinates import (
-    Helioprojective,
     transform_with_sun_center,
     GeocentricSolarEcliptic,
 )
+from sunpy.coordinates.screens import SphericalScreen
 from sunpy.physics.differential_rotation import solar_rotate_coordinate
 from frames import get_helioviewer_frame, get_earth_frame, get_3d_frame
 
@@ -32,9 +32,7 @@ def normalize_hpc(x: float, y: float, event_time: Time, target: Time) -> SkyCoor
             x * u.arcsecond, y * u.arcsecond, frame=get_earth_frame(event_time)
         )
         hv_frame = get_helioviewer_frame(target)
-        with Helioprojective.assume_spherical_screen(
-            hv_frame.observer, only_off_disk=True
-        ):
+        with SphericalScreen(hv_frame.observer, only_off_disk=True):
             return solar_rotate_coordinate(real_coord, hv_frame.observer)
 
 
